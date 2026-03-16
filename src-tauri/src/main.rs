4aaa8422adf1dfe30ab4ab22eb25db592ff8b69b
[dyad] <jbutrum502@gmail.com>
2026-03-16 00:35:53 -0500

    [dyad] Fix tauri_build placement, proper Tauri main, and Windows-only bundling/CI - wrote 4 file(s)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,11 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    tauri_build::build()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
